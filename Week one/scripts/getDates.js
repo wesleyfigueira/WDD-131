aa734a8031e17b5985d5c04b2f3cb33c0e933e57
wesleyfigueira <wesleyfigueira1993@gmail.com>
2024-04-24 09:07:17 -0300

    last modified applied

---
+++ Week one/scripts/getDates.js
@@ -0,0 +1,11 @@
+
+// getdates.js
+
+// Get the current year for copyright
+const currentYear = new Date().getFullYear();
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Output the last modified date
+    const lastModifiedParagraph = document.querySelector('footer p:nth-child(2)');
+    lastModifiedParagraph.textContent = `Last modified: ${document.lastModified} © ${currentYear}`;
+});
